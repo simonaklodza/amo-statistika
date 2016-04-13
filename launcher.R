000831320ca445b0139b6df67d422fe5c63ba713
simonaklodza <simona.klodza@gmail.com>
2016-04-13 14:19:30 +0300

    AMO

--- launcher.R
+++ launcher.R
@@ -1,17 +1,20 @@
 library(knitr)
+library(Cairo)
+library(cairoDevice)
+install.packages("ggplot2")
 
-options(encoding='UTF-8')
+
+#options(encoding='UTF-8')
 
 setwd("/Users/Simona/Documents/GitHub/amo-statistika/")
 
-
 amoNum <- 43
-skolas <- c("Valmieras Valsts gimnazija", "Valmieras Viestura vidusskola")
+skolas <- c("Valmieras Valsts \u01E7imn\u0101zija", "Valmieras Viestura vidusskola")
+theDate <- format(Sys.time(), "%Y-%m-%d")
 
 count <- 0
 skola <- skolas[1]
-#  count <- count + 1
-#  knit2pdf(input="amo43-report.Rnw", output = paste0("amo43-report", count, ".Rnw"))
-knit2pdf("amo43-report.Rnw")
+knit2pdf("amo43-report.Rnw", encoding="UTF8")
+
 
 
